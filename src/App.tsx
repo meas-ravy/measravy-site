1bbcc9f7b27e7d4c8508341ac5e519894f0f6a77
Meas Ravy <jakeson270@gmail.com>
2026-01-03 10:13:57 +0700

    feat: architecture folder

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-black text-white text-3xl">
+        Tailwind v4 is working 🚀
+      </div>
+    </>
+  );
+}
+
+export default App;
